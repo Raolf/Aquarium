f4b88c97dcfe9465e34e2926657e5bf0f04b59cc
Roalf <sebastianbrandt@live.dk>
2018-11-16 10:46:27 +0100

    orange fish slowed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-Run();
+
 
 var fish = document.getElementById("fish1Id");
-var inc;
-//$(fish).animate({width: '188', height:'188'});
+$(fish).animate({width: '188', height:'188'});
+
+Run();
+
 
 function Run() {
-    Bubble();
-    OrangeDblClick();
-    var interval = setInterval(OrangeIdle(),2000);
 
+    var fish = document.getElementById("fish1Id");
     BlueFish();
+    $(fish).stop();
+    Bubble();
+    $(fish).dblclick(function () {
+        var fish = document.getElementById("fish1Id");
+        $(fish).stop();
+        $(fish).animate({left:'-=94', width: '376', top:'-=94', height:'376'}).delay(2000).animate({left:'+=94', width: '188', top:'+=94', height:'188'});
+        
+    });
+    OrangeIdle().delay(1000);
     Run();
 }
 
@@ -27,15 +36,11 @@ function OrangeIdle() {
     var fish = document.getElementById("fish1Id");
     var y = Math.random()*(window.innerHeight-188);
     var x = Math.random()*(window.innerWidth-188);
-    $(fish).animate({top:'-=0',left:'-=0'});
-    $(fish).animate({top:y,left:x});
+    //$(fish).animate({top:'-=0',left:'-=0'});
+    $(fish).animate({top:y,left:x},"slow");
 }
 function OrangeDblClick() {
-    var fish = document.getElementById("fish1Id");
-    $(fish).dblclick(function () {
-        var fish = document.getElementById("fish1Id");
-        $(fish).animate({left:'-=94', width: '376', top:'-=94', height:'376'}).delay(2000).animate({left:'+=94', width: '188', top:'+=94', height:'188'});
-    });
+
 
 }
 

f755c7408cd587e0938fa6784987273792728aaf
Roalf <sebastianbrandt@live.dk>
2018-11-16 09:55:54 +0100

    Bluefish behavior

--- js/script.js
+++ js/script.js
@@ -1,30 +1,16 @@
 Run();
 
 var fish = document.getElementById("fish1Id");
-var dblclick = false;
 var inc;
+//$(fish).animate({width: '188', height:'188'});
 
 function Run() {
+    Bubble();
+    OrangeDblClick();
+    var interval = setInterval(OrangeIdle(),2000);
 
-    $(fish).animate({width: '188', height:'188'});
-    //check();
-
-}
-
-function check() {
-    inc = 100;
-    for (let i = 0; i < inc; i++) {
-
-        $(fish).dblclick(function () {
-            var dblclick = true
-            i = 0;
-        });
-
-        if (inc == 10){
-            i = 0;
-            OrangeIdle();
-        }
-    }
+    BlueFish();
+    Run();
 }
 
 function Bubble() {
@@ -33,22 +19,34 @@ function Bubble() {
     var w = window.innerWidth;
 
     var a = (Math.random() * w);
-    bubble.positionX = a;
-
-    bubble.animate({"height":"+=180", a});
+    $(bubble).positionX = a;
+    $(bubble).positionY = window.innerHeight;
+    $(bubble).animate({top:'-=180'});
 }
 function OrangeIdle() {
-
+    var fish = document.getElementById("fish1Id");
     var y = Math.random()*(window.innerHeight-188);
     var x = Math.random()*(window.innerWidth-188);
+    $(fish).animate({top:'-=0',left:'-=0'});
     $(fish).animate({top:y,left:x});
-    check();
 }
 function OrangeDblClick() {
+    var fish = document.getElementById("fish1Id");
+    $(fish).dblclick(function () {
+        var fish = document.getElementById("fish1Id");
+        $(fish).animate({left:'-=94', width: '376', top:'-=94', height:'376'}).delay(2000).animate({left:'+=94', width: '188', top:'+=94', height:'188'});
+    });
 
-    $(fish).animate({left:'-=94', width: '376', top:'-=94', height:'376'}).delay(2000).animate({left:'+=94', width: '188', top:'+=94', height:'188'});
-    dblclick = false;
-    check();
+}
+
+function BlueFish() {
+    var blueFish = document.getElementById("fish2Id");
+    $(blueFish).mouseover(function () {
+        var blueFish = document.getElementById("fish2Id");
+        var y = Math.random()*(window.innerHeight-188);
+        var x = Math.random()*(window.innerWidth-188);
+        $(blueFish).animate({top:y,left:x})
+    })
 
 }
 
